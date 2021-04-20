17d7c275dbc29d42fb1ef6b4857c08b23117152e
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-19 22:56:45 -0600

    Changed Pokedex number

--- pokemon/pokedex.js
+++ pokemon/pokedex.js
@@ -16,7 +16,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25`).then(
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=151`).then(
         (data) => {
             console.log(data)
         }

81d0468c591e221af66188c804da279173af7beb
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-19 22:32:30 -0600

    Might have forgotten something.

--- pokemon/pokedex.js
+++ pokemon/pokedex.js
@@ -8,7 +8,7 @@ loadPoke.addEventListener('click', () => {
 async function getAPIData(url) {
     try {
         const response = await fetch(url)
-        const data = await response.json
+        const data = await response.json()
         return data
     } catch (error) {
         console.log(error)

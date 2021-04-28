6174f55868b4041d32d7e98526f33d36c5ca8c28
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-28 11:53:43 -0600

    This is the only way it works so far...

--- starwars/moviescript.js
+++ starwars/moviescript.js
@@ -23,18 +23,4 @@ function movieLoop (){
     }    
 }
 
-function shipLoop (){
-    for (let j = 0; j < starships.length; j++) {
-        let myShipTitle = document.createElement("h2");
-        myShipTitle.textContent = starships[j].name;
-
-        let mySpanTag2 = document.createElement("span");
-        mySpanTag2.appendChild(myShipTitle);
-
-        document.querySelector("#ships").appendChild(mySpanTag2);
-    }    
-}
-
 movieLoop();
-
-shipLoop();
--- starwars/shipscript.js
+++ starwars/shipscript.js
@@ -11,6 +11,16 @@ console.log(starships);
 
 console.log(force);
 
+function shipLoop (){
+    for (let j = 0; j < starships.length; j++) {
+        let myShipTitle = document.createElement("h2");
+        myShipTitle.textContent = starships[j].name;
 
+        let mySpanTag2 = document.createElement("span");
+        mySpanTag2.appendChild(myShipTitle);
+
+        document.querySelector("#ships").appendChild(mySpanTag2);
+    }    
+}
 
 shipLoop();

a36e4e147a69382aee69b5f96775706270ef6049
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-28 18:34:51 -0600

    Added more Javascript

--- starwars/moviescript.js
+++ starwars/moviescript.js
@@ -1,26 +1,31 @@
 import { films } from './data/films.js';
-import { starships } from './data/starships.js'
-
-
 
 let force = "May the force be with you";
 
-console.log(films);
-
-console.log(starships);
+let moviewrapper = document.querySelector('#moviewrapper')
 
 console.log(force);
 
 function movieLoop (){
     for (let i = 0; i < films.length; i++) {
+        console.log(`${films[i].episode_id}`)
+
+        const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
+
         let myMovieTitle = document.createElement("h2");
-        myMovieTitle.textContent = films[i].title;
+        myMovieTitle.textContent = foundFilm[i].title;
 
         let mySpanTag1 = document.createElement("span");
         mySpanTag1.appendChild(myMovieTitle);
 
         document.querySelector("#moviewrapper").appendChild(mySpanTag1);
     }    
+}
+
+function getLastNumber(url){
+    let end = url.lastIndexOf('/')
+    let filmNum = end - 1
+    console.log(filmNum)
 }
 
 movieLoop();

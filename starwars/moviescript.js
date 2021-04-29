03f5ba890ee3695be4ffaa9e925521eb31ae3922
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-29 12:16:18 -0600

    Hopefully this works

--- starwars/moviescript.js
+++ starwars/moviescript.js
@@ -2,30 +2,24 @@ import { films } from './data/films.js';
 
 let force = "May the force be with you";
 
-let moviewrapper = document.querySelector('#moviewrapper')
+let movieWrapper = document.querySelector('#moviewrapper')
 
 console.log(force);
 
 function movieLoop (){
     for (let i = 0; i < films.length; i++) {
         console.log(`${films[i].episode_id}`)
-
-        const foundFilm = films.find(film => getLastNumber(film.url) === (i + 1).toString())
-
-        let myMovieTitle = document.createElement("h2");
-        myMovieTitle.textContent = foundFilm[i].title;
-
-        let mySpanTag1 = document.createElement("span");
-        mySpanTag1.appendChild(myMovieTitle);
-
-        document.querySelector("#moviewrapper").appendChild(mySpanTag1);
-    }    
+        const foundMovie = films.find(film => getLastNumber(film.url) === (i + 1).toString())    
+        let movieItem = document.createElement('h2')
+        movieItem.textContent = foundMovie.title
+        movieWrapper
+    }
 }
 
 function getLastNumber(url){
     let end = url.lastIndexOf('/')
-    let filmNum = end - 1
-    console.log(filmNum)
+    let movieNum = end - 1
+    //console.log(movieNum)
 }
 
 movieLoop();

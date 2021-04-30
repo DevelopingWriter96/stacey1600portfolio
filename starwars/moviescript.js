e2c8449323657f661dc046ff99a73b9ac8a07806
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-29 21:10:23 -0600

    I wonder if this will work

--- starwars/moviescript.js
+++ starwars/moviescript.js
@@ -12,7 +12,7 @@ function movieLoop (){
         const foundMovie = films.find(film => getLastNumber(film.url) === (i + 1).toString())    
         let posterFig = document.createElement('figure')
         let figImg = document.createElement('img')
-        figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/${(i + 1)}.jpg`
         let figCap = document.createElement('figcaption')
         figCap.textContent = foundMovie.title
         posterFig.appendChild(figImg)

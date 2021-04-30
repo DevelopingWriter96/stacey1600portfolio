9a5afb167fae79fc75ad2809c86ce6cb12523e39
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-30 17:02:12 -0600

    Test

--- starwars/starships/script.js
+++ starwars/starships/script.js
@@ -7,7 +7,9 @@ const ships = document.querySelector('ships')
 function shipLoop(starships){
     starships.forEach(starship => {
         let shipItem = document.createElement('li')
+        shipItem.textContent = starship.name
 
+        ships.appendChild(shipItem)
     })
 }
 

3a851b8a5024537b2ef780a79976024efb56ffbd
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-04-30 16:31:06 -0600

    Fixed Styles

--- starwars/starships/script.js
+++ starwars/starships/script.js
@@ -1,21 +1,14 @@
 import { starships } from '../data/starships.js'
 
-let force = "May the force be with you";
+console.log(starships.length)
 
-console.log(starships.length);
+const ships = document.querySelector('ships')
 
-console.log(force);
+function shipLoop(starships){
+    starships.forEach(starship => {
+        let shipItem = document.createElement('li')
 
-function shipLoop (){
-    for (let j = 0; j < starships.length; j++) {
-        let myShipTitle = document.createElement("h2");
-        myShipTitle.textContent = starships[j].name;
-
-        let mySpanTag2 = document.createElement("span");
-        mySpanTag2.appendChild(myShipTitle);
-
-        document.querySelector("#ships").appendChild(mySpanTag2);
-    }    
+    })
 }
 
-shipLoop();
+shipLoop(starships);

4d68973f1f6888c584fdb3817dd563dd57fe6b79
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-05-01 15:13:38 -0600

    I hope this works

--- starwars/starships/script.js
+++ starwars/starships/script.js
@@ -1,24 +1,30 @@
 import { starships } from '../data/starships.js'
-import { getLastNumber } from '../utilities/script.js'
+import { getLastNumber, removeChildren } from '../utilities/script.js'
 
 console.log(starships.length)
 
 const ships = document.querySelector('#ships')
 const shipView = document.querySelector('#shipview')
 
-function shipLoop(shipData) {
+function shipLoop(starships) {
     starships.forEach(starship => {
         let shipFig = document.createElement('figure')
         let shipItem = document.createElement('figcaption')
         shipItem.textContent = starship.name
         shipFig.appendChild(shipItem)
-        let shipNum = getLastNumber(shipData.url)
-        let figImg = document.createElement('img')
-        figImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
-        shipFig.appendChild(figImg)
-        shipFig.appendChild(shipItem)
         ships.appendChild(shipFig)
     })
 }
 
+function createShipView(shipData) {
+    removeChildren(shipView)
+    let shipNum = getLastNumber(shipData.url)
+    let figImg = document.createElement('img')
+    figImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    shipFig.appendChild(figImg)
+    shipFig.appendChild(shipItem)
+}
+
 shipLoop(starships);
+
+createShipView();

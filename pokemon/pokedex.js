d363f2a028c6e9f4693b4af158c695fc21b8cb23
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-05-01 19:03:58 -0600

    whoops

--- pokemon/pokedex.js
+++ pokemon/pokedex.js
@@ -83,7 +83,7 @@ function fillCardFront(pokemon) {
     pokeFront.appendChild(frontImage)
     pokemon.types.forEach((pokeType) => {
         let frontType = document.createElement('p')
-        frontType.className - 'type'
+        frontType.className = 'type'
         frontType.textContent = pokeType.type.name
         pokeFront.appendChild(frontType)
         })

ff36ca199ce9ea1e47de62a00cb237019cbef6ad
DevelopingWriter96 <staceyrichard1996@gmail.com>
2021-05-03 21:38:14 -0600

    Scaled back due to long load times.

--- pokemon/pokedex.js
+++ pokemon/pokedex.js
@@ -45,7 +45,7 @@ async function getAPIData(url) {
 }
 
 function loadPage() {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=493`).then(
+    getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=151`).then(
         async (data) => {
             for (const singlePokemon of data.results) {
                 await getAPIData(singlePokemon.url).then(
